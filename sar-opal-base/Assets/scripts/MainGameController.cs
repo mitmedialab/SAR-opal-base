936baa14f6d4a663b061ed18a1c2311c5cc53d0e
jakory <jakory@media.mit.edu>
2015-02-12 18:36:38 -0500

    Added mostly empty Unity project

---
+++ sar-opal-base/Assets/scripts/MainGameController.cs
@@ -0,0 +1,33 @@
+﻿using UnityEngine;
+using System.Collections;
+
+/**
+ * The SAR-opal-base game main controller. Orchestrates everything: 
+ * sets up to receive input via ROS, initializes scenes and creates 
+ * game objecgs based on that input, deals with touch events and
+ * other tablet-specific things.
+ */
+public class MainGameController : MonoBehaviour 
+{
+
+	// Use this for initialization
+	void Start () 
+	{
+	
+	}
+	
+	// Update is called once per frame
+	void Update () 
+	{
+	
+	}
+
+
+	/**
+	 * Instantiate a new game object with the specified properties
+	 */
+	void InstantiateGameObject ()
+	{
+	}
+
+}
